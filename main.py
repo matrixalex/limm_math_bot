049918b7f9661402cca5aaf070af4c0598a8f2ed
KFU-IMM <32076688+KFU-IMM@users.noreply.github.com>
2017-10-13 11:42:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ def handle_solve(message):
     message.text=str(message)
     bot.send_message(message.chat.id, parser.eval_(message.text))
 
-bot.polling()
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
