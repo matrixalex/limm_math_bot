59529242db5fba84b9fa8562c8882ef17434e66f
KFU-IMM <32076688+KFU-IMM@users.noreply.github.com>
2017-10-22 17:40:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from flask import Flask, request
 server = Flask(__name__)
 
 token = S3Connection(os.environ['token'])
+token = '439470650:AAHup458zfpbjGp4c_78E4nMuzcSlRzIcv0'
 bot=telebot.TeleBot(token)
 
 def str(message): #Удаление команды из строки
