eed54a50bc800258382755dc1d8781aabc428d44
KFU-IMM <32076688+KFU-IMM@users.noreply.github.com>
2018-03-24 12:24:36 +0300

    Update bot_app.py

--- bot_app.py
+++ bot_app.py
@@ -59,4 +59,18 @@ def handle_animate(message):
 	photo = open('movie.gif', 'rb')
 	bot.send_document(message.chat.id, photo)
 
-bot.polling(none_stop=True)
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://limmathbot.herokuapp.com/' + token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
