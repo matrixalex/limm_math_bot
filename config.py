44347938f6ba4a6db8e2a6ac3e0af6d08567e489
KFU IMM <refs@protonmail.com>
2018-03-24 16:18:39 +0300

    kl

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
  
-'''from enum import Enum
-from os import environ			'''
+from enum import Enum
+'''from os import environ			'''
 
 token = '414940346:AAFVfcpbR0kKFuQxNayhRckIlRGr0V_2DRE'
 '''hul = environ.get('heroku_token')  '''
