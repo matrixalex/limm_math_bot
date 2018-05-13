a56bdb431d4ded3b7fc970a930b20f5c75e80ede
KFU IMM <refs@protonmail.com>
2018-05-13 19:10:16 +0300

    ,mn

--- bot_app.py
+++ bot_app.py
@@ -16,11 +16,7 @@ import dworker
 
 token=config.token
 bot=telebot.TeleBot(token)
-updates = bot.getUpdates()
-if updates:
-    last_update_id = updates[-1]['update_id']
-    bot.getUpdates(offset=last_update_id+1)
-bot.notifyOnMessage()
+bot.process_new_messages();
 
 
 def str(message): #Удаление команды из строки
