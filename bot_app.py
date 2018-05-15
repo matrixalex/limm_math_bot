41112394b69fb57205048f65776ef8de81556537
KFU IMM <refs@protonmail.com>
2018-05-15 21:49:31 +0300

    print latex

--- bot_app.py
+++ bot_app.py
@@ -91,8 +91,7 @@ def handle_apart(message):
 		message.text=str(message)
 		if(len(message.text)!=0):
 			s=simplify(message.text)
-			lat=sympy.latex(s)
-			plt.text(0, 0.3, r"$%s$" % lat, fontsize = 30, horizontalalignment='center', verticalalignment='center')
+			lat=sympy.latex.print_latex(s)
 			plt.axis('off')
 			plt.savefig('plot.png')
 			bot.send_message(message.chat.id, apart(message.text))
