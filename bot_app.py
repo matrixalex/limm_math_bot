775c7eaef9e2aaaa1ba0285591e222cd6b1015ad
KFU IMM <refs@protonmail.com>
2018-05-15 21:45:49 +0300

    latex

--- bot_app.py
+++ bot_app.py
@@ -91,7 +91,7 @@ def handle_apart(message):
 		message.text=str(message)
 		if(len(message.text)!=0):
 			s=simplify(message.text)
-			lat=sympy.latex.LatexPrinter(s)
+			lat=sympy.latex(s)
 			plt.text(0, 0.3, r"$%s$" % lat, fontsize = 30, horizontalalignment='center', verticalalignment='center')
 			plt.axis('off')
 			plt.savefig('plot.png')
