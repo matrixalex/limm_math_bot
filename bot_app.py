8d6ac8c3678e58c9f965bc5ec0f078799c972fc1
KFU IMM <refs@protonmail.com>
2018-05-15 21:55:11 +0300

    apart

--- bot_app.py
+++ bot_app.py
@@ -91,7 +91,7 @@ def handle_apart(message):
 		message.text=str(message)
 		if(len(message.text)!=0):
 			s=simplify(message.text)
-			lat=sympy.latex.Printer._print(s)
+			lat=sympy.latex.precedence_traditional(s)
 			plt.axis('off')
 			plt.savefig('plot.png')
 			bot.send_message(message.chat.id, apart(message.text))
